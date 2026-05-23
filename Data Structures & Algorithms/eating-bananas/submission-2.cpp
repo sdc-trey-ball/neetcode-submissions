fb2278610831b196199943693e0ba46c448c2d6b
sdc-trey-ball <263561743+sdc-trey-ball@users.noreply.github.com>
2026-05-23 23:34:15 +1200

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+       sort(piles.begin(), piles.end());
+       int low {1}, high = piles.at(piles.size() - 1);
+       int min_speed {piles.at(piles.size() - 1)};
+       while(low <= high) {
+            int m = low + (high - low) / 2;
+            int hours {};
+            for(const int& p : piles){
+                hours += ceil(double(p) / m);
+            } 
+            if(hours <= h) {
+                min_speed = m;
+                high = m -1;
+            } else {
+                low = m + 1;
+            }
+       }
+       return min_speed;
+    }
+};
