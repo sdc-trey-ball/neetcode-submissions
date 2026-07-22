b3092b5badd5276553569e9aa9a567fa1db71364
sdc-trey-ball <263561743+sdc-trey-ball@users.noreply.github.com>
2026-07-22 15:56:56 +1200

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> final;
+    int size;
+    vector<vector<int>> subsets(vector<int>& nums) {
+        size = nums.size();
+        sub(0, vector<int>(), nums);
+        return final;
+    }
+
+    void sub(int i, vector<int> working, const vector<int> nums) {
+        if(i == size) 
+        {
+            cout << working.size() << ' ';
+            final.push_back(vector<int>(working.begin(), working.end()));
+            return; 
+        }
+
+        working.push_back(nums.at(i));
+        sub(i+1, working, nums);
+        working.pop_back();
+        sub(i+1, working, nums);
+    }
+};
