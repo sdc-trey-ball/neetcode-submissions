45f0557380dbd488624a3e5072d872f5095dc223
sdc-trey-ball <263561743+sdc-trey-ball@users.noreply.github.com>
2026-08-03 17:33:52 +1200

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        map<int, int> memo; 
+        memo[0] = 0;
+        memo[1] = 1;
+        memo[2] = 2;
+        for(int i{3}; i <= n; ++i) {
+            memo[i] = memo[i - 1] + memo[i-2];
+        }
+        return memo[n];
+    }
+};
