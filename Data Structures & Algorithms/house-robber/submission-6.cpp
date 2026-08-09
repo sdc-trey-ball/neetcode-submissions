243a1b6416de4890f90b4b2ccf60a48396421c5b
sdc-trey-ball <263561743+sdc-trey-ball@users.noreply.github.com>
2026-08-09 17:53:35 +1200

    Add: house-robber - submission-6

---
+++ Data Structures & Algorithms/house-robber/submission-6.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int bt = 0, bo = 0;
+        for(int i = 0; i < nums.size(); ++i) {
+            int m = max(bo, bt + nums.at(i));
+            bt = bo;
+            bo = m;
+        }
+        return max(bo, bt);
+    }
+};
