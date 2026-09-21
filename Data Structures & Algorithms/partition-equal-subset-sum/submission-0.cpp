0a01fa1751ff93f5d68eda93c724db2348698f8e
sdc-trey-ball <263561743+sdc-trey-ball@users.noreply.github.com>
2026-09-22 07:23:30 +1200

    Add: partition-equal-subset-sum - submission-0

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if(sum % 2 != 0) return false;
+        int partition_sum {sum / 2};
+        int found {0};
+        sort(nums.begin(), nums.end());
+        for(int i{0}; i < nums.size(); ++i) {
+            if(dfs(nums, partition_sum, i, 0)) return true;
+        }
+        return false;
+    }
+
+    bool dfs(const vector<int>& nums, const int target_sum, int i, int sum) {
+        if(sum > target_sum || i == nums.size()) return false;
+        if(sum == target_sum) return true;
+        // iterate
+        int cur = nums.at(i);
+        return dfs(nums, target_sum, i + 1, sum + cur) || dfs(nums, target_sum, i + 1, sum);
+    }
+};
